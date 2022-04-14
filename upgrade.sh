2fdcc45f9e6ff44cdaca6a11668a87cab217b0f0
Stephanos23 <stephen.luu2000@hotmail.com>
2022-04-14 16:52:37 -0400

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Upgrade to v1.5.2
+# Upgrade application
 # Disable and stop control panel service
 echo "Disabling and stopping control application service"
 systemctl disable c3p.service
 systemctl stop c3p.service
 
 # Clear old database files
-echo "Clearing database files"
-rm -f /home/pi/control/h2db/test*
+#echo "Clearing database files"
+#rm -f /home/pi/control/h2db/test*
 
 # Pull new JAR from repo
 echo "Pulling newest files"
